33fe93cf343c1e0d2d0e318ca467cc3897371b04
Abdu919 <abdullahhesn5@gmail.com>
2024-08-27 18:04:14 +0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -6,7 +6,7 @@ links.forEach(link => {
         event.preventDefault(); 
         const url = link.getAttribute('href'); 
 
-        fetch(url)
+        fetch('static/ngrok_url.txt')
             .then(response => response.text())
             .then(html => {
                 mainContainer.innerHTML = html; 
